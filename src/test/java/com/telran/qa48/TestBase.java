73ca3c922113c3b83569ccf4c80598a3854facbf
alinakrivizky <krivickaaa@gmail.com>
2025-09-13 23:07:30 +0300

    add1

--- src/test/java/com/telran/qa48/TestBase.java
+++ src/test/java/com/telran/qa48/TestBase.java
@@ -2,13 +2,18 @@ package com.telran.qa48;
 
 import com.shop.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.lang.reflect.Method;
 
 public class TestBase {
 
     protected ApplicationManager app = new ApplicationManager(System.getProperty("browser", Browser.CHROME.browserName()));
-
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
     @BeforeMethod
         public void setUp() {
         app.init();
@@ -17,6 +22,20 @@ public class TestBase {
     @AfterMethod
         public void tearDown() {
         app.stop();
+    }
+    @BeforeMethod
+    public void startTest(Method method) {
+        logger.info("Start test"+ method.getName());
+    }
+    @AfterMethod
+    public void stopTest(ITestResult result) {
+        if(result.isSuccess()) {
+            logger.info("PASSED: "+ result.getMethod().getMethodName());
+        }else {
+            logger.error("FAILED: "+ result.getMethod().getMethodName());
+        }
+        logger.info("Stop test");
+        logger.info("*****************");
     }
 
 }
